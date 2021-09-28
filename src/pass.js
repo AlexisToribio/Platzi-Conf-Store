9907e69cd77958852d7875e1ea0586e6dfceef63
Alexis Toribio <alexistoba21@hotmail.com>
2021-09-28 14:30:18 -0500

    [add] firebase config

--- src/pass.js
+++ src/pass.js
@@ -2,6 +2,7 @@ const pass = {
   paypalPaymentClientID: String(process.env.PAYPAL_PAYMENT_CLIENT_ID),
   googleMapsAPI: String(process.env.GOOGLE_MAPS_API),
   geocodingMaps: String(process.env.GEOCODING_MAPS),
+  firebaseToken: String(process.env.FIREBASE_TOKEN),
 };
 
 export default pass;
